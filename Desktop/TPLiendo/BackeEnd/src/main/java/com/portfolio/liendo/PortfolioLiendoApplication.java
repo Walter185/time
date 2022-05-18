f510bd2e003c28864a73d9782da8e26147deb622
Walter185 <walter.liendo@gmail.com>
2022-05-18 10:17:50 -0300

    back

---
+++ Desktop/TPLiendo/BackeEnd/src/main/java/com/portfolio/liendo/PortfolioLiendoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.liendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioLiendoApplication {
+
+	public static void main(String[] args) {
+    SpringApplication.run(PortfolioLiendoApplication.class, args);
+	}
+
+}
